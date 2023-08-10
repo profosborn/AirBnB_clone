8c857cb1c8add8d87b3aaccd561f2f4d305f3358
Akuna Joshua <akunajoshua@gmail.com>
2023-08-10 16:46:05 +0100

    Task 9. More classes! - implemented the State model class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""The module contains the class State which is a subclass of the BaseModel"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Task 9.
+    The class is a subclass of the Baseodel class with the
+        additional attributes defined
+        a) name: string-empty string
+    """
+    name = ''
