e36458c1a67cf50fe7dc1be7ec98a172d6a3dd19
Akuna Joshua <akunajoshua@gmail.com>
2023-08-13 21:49:37 +0100

    models/base_model.py refactored

--- models/base_model.py
+++ models/base_model.py
@@ -25,17 +25,12 @@ class BaseModel:
             instance of the BaseModel instead.
         """
         if kwargs and len(kwargs) > 0:
-            kwargs.pop('__class__', None)
-            kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'],
-                    "%Y-%m-%dT%H:%M:%S.%f"
-                    )
-            kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'],
-                    "%Y-%m-%dT%H:%M:%S.%f"
-                    )
+            fmt = "%Y-%m-%dT%H:%M:%S.%f"
             for key, val in kwargs.items():
-                setattr(self, key, val)
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, fmt)
+                else:
+                    self.__dict__[key] = val
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
